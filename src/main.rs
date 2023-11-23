7d563c2e26d9ff6df7e0aabf02e099f8cf86ff9a
Slope <linearfunc@proton.me>
2023-11-23 11:41:12 +0000

    Basic solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    for i in 1..100 {
+        if i % 3 == 0 && i % 5 == 0 {
+            println!("fizzbuzz");
+        } else if i % 3 == 0 {
+            println!("fizz");
+        } else if i % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", i);
+        }
+    }
 }
