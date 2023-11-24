a5e2909fde5ee43be95e47c5059f0c8ab78e7565
Slope <linearfunc@proton.me>
2023-11-24 19:55:49 +0000

    Added builder methods

--- src/builder.rs
+++ src/builder.rs
@@ -23,4 +23,28 @@ impl<I: FizzBuzzable<O>, O: FizzBuzzed<I>> FizzBuzzBuilder<I, O> {
     pub fn new() -> Self {
         Self::default()
     }
+
+    pub fn start_at(mut self, start: I) -> Self {
+        self.start = Some(start);
+        self
+    }
+
+    pub fn end_at(mut self, end: I) -> Self {
+        self.end = Some(end);
+        self
+    }
+
+    pub fn add_match(mut self, input: I, output: O) -> Self {
+        match &mut self.map {
+            Some(m) => { m.insert(input, output); }
+            None => { self.map = Some(BTreeMap::from([(input, output)])); }
+        }
+
+        self
+    }
+
+    pub fn set_rule(mut self, rule: impl Fn(I, I) -> bool + 'static) -> Self {
+        self.rule = Some(Box::new(rule));
+        self
+    }
 }
--- src/traits/fizzbuzzable.rs
+++ src/traits/fizzbuzzable.rs
@@ -1,6 +1,6 @@
 use super::FizzBuzzed;
 
-pub trait FizzBuzzable<O: FizzBuzzed<Self>>: Clone + PartialEq + PartialOrd {
+pub trait FizzBuzzable<O: FizzBuzzed<Self>>: Clone + PartialEq + Eq + PartialOrd + Ord {
     fn succ(&self) -> Self;
     fn pred(&self) -> Self;
 }
