7c78851760b8129114fcaea8d9912ae7148b9358
Slope <linearfunc@proton.me>
2024-02-09 21:43:22 +0000

    Updated traits

--- src/traits/fizzbuzzable.rs
+++ src/traits/fizzbuzzable.rs
@@ -1,6 +1,7 @@
 use super::FizzBuzzed;
+use std::fmt::{Display, Debug};
 
-pub trait FizzBuzzable<O: FizzBuzzed<Self>>: Clone + PartialEq + Eq + PartialOrd + Ord {
+pub trait FizzBuzzable<O: FizzBuzzed<Self>>: Clone + PartialEq + Eq + PartialOrd + Ord + Display + Debug + Sized {
     fn min() -> Option<Self>;
     fn max() -> Option<Self>;
     fn succ(&self) -> Self;
--- src/traits/fizzbuzzed.rs
+++ src/traits/fizzbuzzed.rs
@@ -1,7 +1,7 @@
 use super::FizzBuzzable;
 use std::collections::BTreeMap;
-use std::fmt;
+use std::fmt::{Display, Debug};
 
-pub trait FizzBuzzed<I: FizzBuzzable<Self>>: fmt::Display + Sized + Clone {
+pub trait FizzBuzzed<I: FizzBuzzable<Self>>: Display + Debug + Sized + Clone {
     fn from(n: I, map: &BTreeMap<I, Self>, rule: &impl Fn(I, I) -> bool) -> Vec<Self>;
 }
