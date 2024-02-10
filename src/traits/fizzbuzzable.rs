3e56aeaa372cf447244f702b2e05a903c3de1851
Slope <linearfunc@proton.me>
2024-02-10 20:08:39 +0000

    Fixed error handling

--- src/traits/fizzbuzzable.rs
+++ src/traits/fizzbuzzable.rs
@@ -1,6 +1,9 @@
 use super::FizzBuzzed;
 use std::fmt::{Debug, Display};
 
+// TODO: change FizzBuzzable to be an iterator
+// use a builder to automatically generate iterators 
+
 pub trait FizzBuzzable<O: FizzBuzzed<Self>>:
     Clone + PartialEq + Eq + PartialOrd + Ord + Display + Debug + Sized
 {
