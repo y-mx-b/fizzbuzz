d98e74647ac5f369e9d9cda91c00aa04d669b438
Slope <linearfunc@proton.me>
2024-02-10 20:08:09 +0000

    Revert "Removed unnecessary error cases"
    This reverts commit f6496d0967b63a0a24bcea9075721934cf846ede.

--- src/builder/error.rs
+++ src/builder/error.rs
@@ -3,7 +3,12 @@ use std::fmt;
 
 #[derive(Debug)]
 pub enum FizzBuzzBuilderError {
+    MissingMap,
+    MissingRule,
+    MissingRuleAndMap,
     InvalidUnboundedStart,
+    InvalidUnboundedEnd,
+    InvalidUnboundedBounds,
 }
 
 // TODO: write better error messages with more context
@@ -13,7 +18,12 @@ impl fmt::Display for FizzBuzzBuilderError {
             f,
             "{}",
             match self {
+                Self::MissingMap => "Please provide an appropriate map.",
+                Self::MissingRule => "Please provide an appropriate rule.",
+                Self::MissingRuleAndMap => "Please provide an appropriate rule and map.",
                 Self::InvalidUnboundedStart => "Unbounded start is invalid.",
+                Self::InvalidUnboundedEnd => "Unbounded end is invalid.",
+                Self::InvalidUnboundedBounds => "Unbounded bounds are invalid.",
             }
         )
     }
